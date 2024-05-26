4d72f5b0a4590628dc159d255629f76a120d8f6b
ankur0904 <ankursingh91002@gmail.com>
2024-05-27 00:36:52 +0530

    Added unordered_map

--- maps.cpp
+++ maps.cpp
@@ -1,18 +1,19 @@
 # include <iostream>
 # include <map>
 #include <string>
+# include <unordered_map>
 
 using namespace std;
-void print(map<int, string> &m);
+void print(unordered_map<int, int> &m);
 int main(){
-    map<int, string> m; // sorted order
-    // INSERTION  log(n) and ACCESS O(logn)
-    m[1] = "abc";   // O(logn) time complexity
-    m[5] = "cdc";
-    m[3] = "efg";
-    m[4]; // insert the default value
-    m.insert({2, "hij"});
-    m.insert(make_pair(4, "klm"));
+    // map<int, string> m; // sorted order, uses red black tree
+    // // INSERTION  log(n) and ACCESS O(logn)
+    // m[1] = "abc";   // O(logn) time complexity
+    // m[5] = "cdc";
+    // m[3] = "efg";
+    // m[4]; // insert the default value
+    // m.insert({2, "hij"});
+    // m.insert(make_pair(4, "klm"));
 
     // Note: map keys are unique
 
@@ -21,28 +22,39 @@ int main(){
     //     cout << (*it).first << " " << it -> second << endl;
     // }
 
-    auto it = m.find(3); // O(logn)
-    if(it != m.end()){
-        cout << "value: " << it->first << " " << it->second << endl;
-    }else{
-        cout << "Not found" << endl;
-    }
+    // auto it = m.find(3); // O(logn)
+    // if(it != m.end()){
+    //     cout << "value: " << it->first << " " << it->second << endl;
+    // }else{
+    //     cout << "Not found" << endl;
+    // }
 
-    auto itr = m.find(3);
+    // auto itr = m.find(3);
     // m.erase(3); // O(logn)
-    if (itr != m.end())
-    {
-        m.erase(itr);   // O(logn)
-    }
+    // if (itr != m.end())
+    // {
+    //     m.erase(itr);   // O(logn)
+    // }
     // m.erase(itr);
-    m.clear();
-    print(m);
+    // m.clear();
+    // print(m);
 
-    map<string, string> mp;
-    mp["abc"] = "def"; // Insertion time: s.size() * O(logn)
+    // map<string, string> mp;
+    // mp["abc"] = "def"; // Insertion time: s.size() * O(logn)
+
+    unordered_map<int, int> u_mp;  // uses hash table
+    // INSERTION O(1) and ACCESS O(1) TIME
+    // DELETE O(1) TIME
+    // *valid keys datapoint* those data types which have hash function defined internally e.g. int, string, float, char, long long etc.
+    u_mp[1] = 100;
+    u_mp[4] = 400;
+    u_mp[2] = 200;
+    u_mp[3] = 300;
+    print(u_mp);
+
 }
 
-void print(map<int, string> &m){
+void print(unordered_map<int, int> &m){
     cout << "Size: " << m.size() << endl;
     for(auto &pr: m){
         cout << pr.first << " " << pr.second << endl;
